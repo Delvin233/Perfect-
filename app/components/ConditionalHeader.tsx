2ca7d55fb91565da0c509c07566f7999de433fe7
Delvin233 <yamoahdelvin@gmail.com>
2025-12-10 01:20:04 +0000

    fix: remove unused imports in ConditionalHeader

--- app/components/ConditionalHeader.tsx
+++ app/components/ConditionalHeader.tsx
@@ -1,16 +1,6 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-import Header from "./Header";
-
 export default function ConditionalHeader() {
-  const pathname = usePathname();
-
-  // Hide header on home page (where arcade menu is)
-  if (pathname === "/") {
-    return null;
-  }
-
-  // Show header on all other pages
-  return <Header />;
+  // Hide header on all pages for full arcade experience
+  return null;
 }
