4e58691456ed264faca6b3845e7a519c776a5520
Delvin233 <yamoahdelvin@gmail.com>
2025-12-10 03:26:21 +0000

    feat: added top and button padding to main menu

--- app/components/EnhancedMainMenu.tsx
+++ app/components/EnhancedMainMenu.tsx
@@ -197,7 +197,7 @@ export default function EnhancedMainMenu({
           intensity="medium"
         />
 
-        <div className="absolute inset-0 z-10 flex flex-col justify-center">
+        <div className="absolute inset-0 z-10 flex flex-col justify-center pt-8 pb-8">
           {/* Enhanced header with glow effect */}
           <div
             className={`flex items-center justify-between mb-4 px-6 lg:px-12 ${isLoaded ? "enhanced-fade-in" : "opacity-0"}`}
